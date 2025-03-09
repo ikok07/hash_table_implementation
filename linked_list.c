b1c6cab29587bd043d75504910075d45a17ccfe3
Kaloyan Stefanov <kokmarok@gmail.com>
2025-03-09 20:18:53 +0200

    add linked list

---
+++ linked_list.c
@@ -0,0 +1,74 @@
+//
+// Created by Kok on 3/9/25.
+//
+
+#include <stdlib.h>
+
+#include "linked_list.h"
+
+linked_list_t *list_allocate() {
+    linked_list_t *list = malloc(sizeof(linked_list_t));
+    return list;
+}
+
+linked_list_item_t *list_read(linked_list_t *list, int item_id) {
+    if (list == NULL) return NULL;
+
+    linked_list_t *curr_node = list;
+
+    while (curr_node != NULL) {
+        if (curr_node->item != NULL && curr_node->item->id == item_id) return curr_node->item;
+        curr_node = curr_node->next;
+    }
+
+    return NULL;
+}
+
+linked_list_t *list_insert(linked_list_t *list, linked_list_item_t *item) {
+    if (list == NULL) {
+        linked_list_t *head = list_allocate();
+        head->item = item;
+        head->next = NULL;
+        list = head;
+        return list;
+    }
+
+    linked_list_t *new_node = list_allocate();
+    if (new_node == NULL) return NULL;
+
+    new_node->item = item;
+    new_node->next = list;
+
+    return new_node;
+}
+
+linked_list_t *list_remove(linked_list_t *list, int item_id) {
+    if (list == NULL) return NULL;
+
+    linked_list_t *prev_node = NULL;
+    linked_list_t *curr_node = list;
+
+    while (curr_node != NULL) {
+        if (curr_node->item && curr_node->item->id == item_id) {
+            if (prev_node == NULL) list = curr_node->next;
+            else prev_node->next = curr_node->next;
+            list_node_free(curr_node);
+            return list;
+        }
+        prev_node = curr_node;
+        curr_node = curr_node->next;
+    }
+
+    return list;
+}
+
+void list_node_free(linked_list_t *node) {
+    if (node == NULL) return;
+
+    if (node->item != NULL) {
+        if (node->item->value != NULL) free(node->item->value);
+        free(node->item);
+    };
+
+    free(node);
+}
---
+++ linked_list.h
@@ -0,0 +1,25 @@
+//
+// Created by Kok on 3/9/25.
+//
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+typedef struct {
+    int id;
+    void *value;
+} linked_list_item_t;
+
+typedef struct linked_list {
+    linked_list_item_t *item;
+    struct linked_list *next;
+} linked_list_t;
+
+linked_list_t *list_allocate();
+linked_list_item_t *list_read(linked_list_t *list, int item_id);
+linked_list_t *list_insert(linked_list_t *list, linked_list_item_t *item);
+linked_list_t *list_remove(linked_list_t *list, int item_id);
+
+void list_node_free(linked_list_t *list);
+
+#endif //LINKED_LIST_H
